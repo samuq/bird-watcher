623be51e9f80a187bbc0d807245433ddd462f116
Samuli Kuusisto <samulikuusisto@Samuli-Air.home>
2018-07-08 15:11:09 +0300

    ..

--- server.js
+++ server.js
@@ -4,24 +4,7 @@ const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-  app.use(forceSSL());
+
 app.use(express.static(__dirname + '/dist/bird-watcher'));
 // Start the app by listening on the default
 // Heroku port
