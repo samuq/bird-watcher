78f885cbc819604bace3f978ea7b7ddde288e63d
Samuli Kuusisto <samulikuusisto@Samuli-Air.home>
2018-07-08 16:53:41 +0300

    ..

--- src/app/services/counters.service.ts
+++ src/app/services/counters.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { map } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
 export class CountersService {
-
   constructor(private http: HttpClient) { }
   configUrl = '/counters.json';
 
   getCounters() {
-    return this.http.get(this.configUrl);
+    return this.http.get(this.configUrl).pipe(map((response) => response))
+    .subscribe((status) => console.log(`status = ${status}`));;
   }
 }
