43f3ec21b3fa000900f05c5e2d81098ab698b869
Samuli Kuusisto <samulikuusisto@Samuli-Air.home>
2018-07-08 17:15:58 +0300

    create endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const pathExists = require('path-exists');
 // Run the app by serving the static files
 // in the dist directory
 
@@ -9,7 +10,19 @@ app.use(express.static(__dirname + '/dist/bird-watcher'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 4200);
+app.get('/counters', function(req, res) {
+
+    res.send(getCounters());
+  });
 app.get('/*', function(req, res) {
     console.log(__dirname);
     res.sendFile(path.join(__dirname + '/dist/bird-watcher/index.html'));
   });
+configUrl = '/counters.json';
+
+function getCounters() {
+    return pathExists(this.configUrl).then(function (exists) {
+        console.log('exists');
+        console.log(exists);
+    });
+}
--- src/app/services/counters.service.ts
+++ src/app/services/counters.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
 export class CountersService {
   constructor(private http: HttpClient) { }
-  configUrl = '/counters.json';
+  configUrl = '/counters';
 
   getCounters() {
     return this.http.get(this.configUrl)
-    .subscribe((status) => console.log(`status = ${status}`));;
+    .subscribe((status) => console.log(status);;
   }
 }
