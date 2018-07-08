6a5f4958259f33069a77264b0fd99bae4f288352
Samuli Kuusisto <samulikuusisto@Samuli-Air.home>
2018-07-08 16:01:59 +0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { CounterListComponent } from './components/counter-list/counter-list.component';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterListComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
