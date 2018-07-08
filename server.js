ea16d017ceb08617aeb5c685a6985132aee0b831
Samuli Kuusisto <samulikuusisto@Samuli-Air.home>
2018-07-08 14:54:24 +0300

    ...

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
     console.log(__dirname);
-    res.sendFile(path.join('/dist/bird-watcher/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
