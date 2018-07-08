a4c37c91bc680f824e1655b17dd13a14c71287da
Samuli Kuusisto <samulikuusisto@Samuli-Air.home>
2018-07-08 16:07:12 +0300

    Modify component and module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,18 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { CounterListComponent } from './components/counter-list/counter-list.component';
 import { ReportComponent } from './components/report/report.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes: Routes = [
+  { path: 'report', component: ReportComponent },
+  {
+    path: 'counters',
+    component: CounterListComponent
+  },
+  { path: '',
+    redirectTo: '/counters',
+    pathMatch: 'full'
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +25,10 @@ import { ReportComponent } from './components/report/report.component';
     ReportComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
